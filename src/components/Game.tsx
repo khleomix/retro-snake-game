f0a02a24808bc9c13c3002da1ad92c79593cab28
JC <jc@webdevstudios.com>
2022-07-10 16:31:02 +0800

    Style cleanup

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -25,18 +25,17 @@ const initialState = {
 }
 
 
-const Arcade = () => {
-  	//const [play, { stop }] = useSound('https://www.learningcontainer.com/wp-content/uploads/2020/02/Kalimba.mp3');
-  	const [play, { stop }] = useSound('https://github.com/khleomix/retro-snake-game/blob/feature/snake-styles/public/start.mp3');
-
-  	return (
-    	<button onMouseEnter={() => play()} onMouseLeave={() => stop()}>
-      		<span role="img" aria-label="trumpet">
-        		🎺
-      		</span>
-    	</button>
-  	);
-};
+//const Arcade = () => {
+//  	const [play, { stop }] = useSound('https://www.learningcontainer.com/wp-content/uploads/2020/02/Kalimba.mp3');
+//
+//  	return (
+//    	<button onMouseEnter={() => play()} onMouseLeave={() => stop()} className="btn-primary bg-red-100 hover:bg-red-200 focus:bg-red-200 box-border text-white font-bold text-2xl m-4 min-w-[100px] p-4 uppercase w-auto transition-all duration-75" >
+//      		<span role="img" aria-label="trumpet">
+//        		🎺
+//      		</span>
+//    	</button>
+//  	);
+//};
 
 
 class Game extends React.Component {
@@ -175,7 +174,7 @@ class Game extends React.Component {
 		  		}
 
 		  		<div className="flex my-2 justify-center">
-					<button className="btn-primary bg-red-100 hover:bg-red-200 focus:bg-red-200 box-border text-white font-bold text-xl m-4 min-w-[100px] p-4 uppercase w-auto transition-all duration-75" onClick={ () => {
+					<button className="btn-primary bg-red-100 hover:bg-red-200 focus:bg-red-200 box-border text-white font-bold text-3xl m-4 min-w-[100px] p-4 uppercase w-auto transition-all duration-75" onClick={ () => {
 						if ( this.state.play ) {
 							this.setState( initialState );
 						} else this.setState( {play: true} )
@@ -183,13 +182,12 @@ class Game extends React.Component {
 					}>{this.state.play ? "End Game" : "Play Game"}</button>
 
 					{this.state.play ?
-						<button className={`ml-2 btn-secondary box-border text-white font-bold text-xl m-4 min-w-[100px] p-4 uppercase w-auto transition-all duration-75 ${this.state.pause ? "bg-gray-200 hover:bg-gray-100 focus:bg-gray-100" : "bg-blue-100 hover:bg-blue-200 focus:bg-blue-200"}`} onClick={ () => {
+						<button className={`ml-2 btn-secondary box-border text-white font-bold text-3xl m-4 min-w-[100px] p-4 uppercase w-auto transition-all duration-75 ${this.state.pause ? "bg-gray-200 hover:bg-gray-100 focus:bg-gray-100" : "bg-blue-100 hover:bg-blue-200 focus:bg-blue-200"}`} onClick={ () => {
 							this.setState( {pause: this.state.pause ? false : true} )
 						}}>{this.state.pause ? "Resume" : "Pause Game"}</button>
 					:
 						<></>
 					}
-					<Arcade />
 				</div>
 	  		</div>
 		)
