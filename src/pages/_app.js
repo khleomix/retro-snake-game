3348b3af03504ab5334602da02e7fccbf02bb415
JC <jc@webdevstudios.com>
2022-07-12 03:58:12 +0800

    Add meta tags and title

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,21 @@
+import Head from 'next/head'
 import '../styles/globals.css'
 
 function RetroSnake( { Component, pageProps } ) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta charset="UTF-8" />
+        <title>Retro Snake Game</title>
+        <meta name="description" content="A Nextjs recreation of the snake game we all played and loved back in the day." />
+        <meta name="keywords" content="snake game, retro, nextjs, reactjs" />
+        <meta name="author" content="JC Palmes" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default RetroSnake
