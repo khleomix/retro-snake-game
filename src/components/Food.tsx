247037ad8c19b16306c6d49b36e7b7ab065feaf9
JC <jc@webdevstudios.com>
2022-07-12 04:09:17 +0800

    Fix food svg location

--- src/components/Food.tsx
+++ src/components/Food.tsx
@@ -8,7 +8,7 @@ export default function Food(props: FoodProps) {
         top: `${props.dot[1]}%`
     }
     return (
-        <div className="snake-food absolute w-4 h-4 block before:h-4 before:content-[url('../../public/food.svg')] before:-left-[6px] before:absolute before:-top-[17px] before:w-4" style={styleFood}>
+        <div className="snake-food absolute w-4 h-4 block before:h-4 before:content-[url('/food.svg')] before:-left-[6px] before:absolute before:-top-[17px] before:w-4" style={styleFood}>
         </div>
     )
 }
